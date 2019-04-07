d415f608d1d7b3d1fcf4a0e655708e4afff7bde1
indexraja <48822608+indexraja@users.noreply.github.com>
2019-04-07 21:24:10 +0530

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 10:54:01 2019
+
+@author: rajak
+"""
+
+from keras.preprocessing.image import img_to_array
+import imutils
+import cv2
+from keras.models import load_model
+import numpy as np
+
+detection_model_path = 'C:/Users/rajak/Desktop/haar.xml'
+emotion_model_path = 'C:/Users/rajak/Desktop/_mini_XCEPTION.106-0.65.hdf5'
+
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
+ "neutral"]
+
+
+# starting video streaming
+cv2.namedWindow('your_face')
+camera = cv2.VideoCapture(0)
+while True:
+    frame = camera.read()[1]
+    #reading the frame
+    frame = imutils.resize(frame,width=600)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=4,minSize=(28,28),flags=cv2.CASCADE_SCALE_IMAGE)
+    
+    canvas = np.zeros((500, 500, 3), dtype="uint8")
+    frameClone = frame.copy()
+    if len(faces) > 0:
+        faces = sorted(faces, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces
+     
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (48, 48))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        
+        
+        preds = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(preds)
+        label = EMOTIONS[preds.argmax()]
+    else: continue
+ 
+    for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+        # construct the label text
+        text = "{}: {:.2f}%".format(emotion, prob * 100)
+        w = int(prob * 300)
+        cv2.rectangle(canvas, (7, (i * 35) + 5),
+        (w, (i * 35) + 35), (0, 0, 255), -1)
+        cv2.putText(canvas, text, (10, (i * 35) + 23),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+        (255, 255, 255), 2)
+        cv2.putText(frameClone, label, (fX, fY - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
+                      (0, 0, 255), 2)
+
+    cv2.imshow('your_face', frameClone)
+    cv2.imshow("Probabilities", canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
